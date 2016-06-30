27e005890442dcd3f9f6819aebafafb6b57072c1
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2016-06-30 21:30:42 +0100

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+readme = open('README.md').read()
+version = "0.1.0"
+
+setup(name="pyvol",
+      packages=["pyvol", "pyvol.external", "pyvol.io", "pyvol.mesh", "pyvol.shaders"],
+      version=version,
+      description="Python package for creating custom 3D viewers",
+      long_description=readme,
+      license='MIT',
+      keywords=["bioimage", "3D", "visualiser"],
+      install_requires=[
+        "numpy",
+        "scipy",
+        "Pillow",
+        "PyOpenGL",
+        "PyOpenGl-accelerate",
+      ]
+)
