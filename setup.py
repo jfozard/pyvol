bbf27265e8ce15598c075efb3e3c08ca7671b44f
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2016-07-01 13:50:02 +0100

    Fixed defect in setup.py file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ readme = open('README.md').read()
 version = "0.1.0"
 
 setup(name="pyvol",
-      packages=["pyvol", "pyvol.external", "pyvol.io", "pyvol.mesh", "pyvol.shaders"],
+      packages=["pyvol", "pyvol.external", "pyvol.parser", "pyvol.mesh", "pyvol.shaders"],
       version=version,
       description="Python package for creating custom 3D viewers",
       long_description=readme,
